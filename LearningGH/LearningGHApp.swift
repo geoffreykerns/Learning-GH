364b34becf7f70e346d5d4269d32f942acccf6b5
Geoffrey Kerns <geoffreykerns@gmail.com>
2021-12-13 15:52:15 -0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Geoffrey Kerns on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
